09fe9bb822b3c07c231f269a6654014611381603
Krishnansh5 <97296908+Krishnansh5@users.noreply.github.com>
2022-03-31 12:28:39 +0530

    Update Q2JS.js

--- Q2/Q2JS.js
+++ Q2/Q2JS.js
@@ -4,66 +4,69 @@ async function getData(url) {
     return responseText;
 }
 
-var UL_PlanetData = document.getElementById("PlanetData");
+function LoadData() {
+    var UL_PlanetData = document.getElementById("PlanetData");
 
-getData('https://swapi.dev/api/planets/1/').then((data) => {
-    for (DataKey in data) {
-        // in case DataKey=residents/films form a nested list of urls 
-        if (DataKey == "residents") {
+    getData('https://swapi.dev/api/planets/1/').then((data) => {
+        for (DataKey in data) {
+            // in case DataKey=residents/films form a nested list of urls 
+            if (DataKey == "residents") {
+                // create and append li element
+                var Li = document.createElement("li")
+                Li.innerHTML = "<p class='key'>" + String(DataKey) + "</p>"
+                UL_PlanetData.appendChild(Li);
+                // make a sublist of residents
+                var UL_Res = document.createElement("ul");
+                Li.appendChild(UL_Res);
 
-            var Li = document.createElement("li")
-            Li.innerHTML ="<p class='key'>"+ String(DataKey) + "</p>"
-            UL_PlanetData.appendChild(Li);
-
-            var UL_Res = document.createElement("ul");
-            Li.appendChild(UL_Res);
-
-            // async func to get resident data and print while working in bg
-            async function printResidents() {
-                for (Resident_url of data.residents) {
-                    var Res_Li = document.createElement("li");
-                    await getData(Resident_url).then((Resdata) => {
-                        return Resdata.name;
-                    }).then((ResidentName) => {
-                        Res_Li.innerHTML = "<a class='urls' href='" + Resident_url + "'>" + ResidentName + "</a>";
-                        UL_Res.appendChild(Res_Li);
-                    })
+                // async func to get names of residents and print while rest of page loads
+                async function printResidents() {
+                    for (Resident_url of data.residents) {
+                        var Res_Li = document.createElement("li");
+                        await getData(Resident_url).then((Resdata) => {
+                            return Resdata.name;
+                        }).then((ResidentName) => {
+                            Res_Li.innerHTML = "<a class='urls' href='" + Resident_url + "'>" + ResidentName + "</a>";
+                            UL_Res.appendChild(Res_Li);
+                        })
+                    }
                 }
+                printResidents();
             }
-            printResidents();
-        }
-        else if (DataKey == "films") {
+            else if (DataKey == "films") {
+                // create and append li element
+                var Li = document.createElement("li")
+                Li.innerHTML = "<p class='key'>" + String(DataKey) + "</p>"
+                UL_PlanetData.appendChild(Li);
+                 // make a sublist of films
+                var UL_Films = document.createElement("ul");
+                Li.appendChild(UL_Films);
 
-            var Li = document.createElement("li")
-            Li.innerHTML = "<p class='key'>"+String(DataKey) + "</p>"
-            UL_PlanetData.appendChild(Li);
-
-            var UL_Films = document.createElement("ul");
-            Li.appendChild(UL_Films);
-
-            // async func to get film data and print while working in bg
-            async function printFilms() {
-                for (filmUrl of data.films) {
-                    var Film_Li = document.createElement("li");
-                    await getData(filmUrl).then((Filmdata) => {
-                        return Filmdata.title;
-                    }).then((FilmName) => {
-                        Film_Li.innerHTML = "<a class='urls' href='" + filmUrl + "'>" + FilmName + "</a>";
-                        UL_Films.appendChild(Film_Li);
-                    })
+                // async func to get film names and print while rest of page loads
+                async function printFilms() {
+                    for (filmUrl of data.films) {
+                        var Film_Li = document.createElement("li");
+                        await getData(filmUrl).then((Filmdata) => {
+                            return Filmdata.title;
+                        }).then((FilmName) => {
+                            Film_Li.innerHTML = "<a class='urls' href='" + filmUrl + "'>" + FilmName + "</a>";
+                            UL_Films.appendChild(Film_Li);
+                        })
+                    }
                 }
+                printFilms();
             }
-            printFilms();
+            else if (DataKey == "url") {
+                let Li = document.createElement("li");
+                Li.innerHTML = "<p class='key'>" + String(DataKey) + "  :  </p><a class='urls' href='" + data[DataKey] + "'>Link to the JSON file</a>";
+                UL_PlanetData.appendChild(Li);
+            }
+            else {
+                let Li = document.createElement("li");
+                Li.innerHTML = "<p class='key'>" + String(DataKey) + " : </p>" + "<p class='value'>" + data[DataKey] + "</p>";
+                UL_PlanetData.appendChild(Li);
+            }
+
         }
-        else if (DataKey == "url") {
-            let Li = document.createElement("li");
-            Li.innerHTML ="<p class='key'>" + String(DataKey) +"  :  </p><a class='urls' href='" + data[DataKey] + "'>Link to the JSON file</a>";
-            UL_PlanetData.appendChild(Li);
-        }
-        else {
-            let Li = document.createElement("li");
-            Li.innerHTML = "<p class='key'>" + String(DataKey) +" : </p>" +"<p class='value'>"+ data[DataKey]+"</p>";
-            UL_PlanetData.appendChild(Li);
-        }
-    }
-})
+    })
+}
